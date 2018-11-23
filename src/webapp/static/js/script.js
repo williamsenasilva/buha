76e9a09748b5c8f1c139deb6b74bbfbf5f8c5fd6
Andre Barbosa <an.barbosa@outlook.com.br>
2018-11-23 13:00:40 -0200

    Inserido return false para que o firefox não redirecione a navegação, impedindo a requisição ajax de concretizar quando tentamos inserir um estudante pelo formulario. (BUG FIREFOX).

--- src/webapp/static/js/script.js
+++ src/webapp/static/js/script.js
@@ -6,8 +6,9 @@ function insertStudent() {
   student["name"] = $("#input-name").val();  
   student["university"] = $("#input-university").val();  
   student["academicID"] = $("#input-academic-id").val();  
+  //alert("Inserindo estudante "+student["name"]+" "+student["university"]+" "+student["academicID"])
   $.ajax({
-    type: "POST",
+    type: "post",
     url: "/api/insert-student",
     data: JSON.stringify(student, null, '\t'),
     contentType: 'application/json;charset=UTF-8',
@@ -16,7 +17,12 @@ function insertStudent() {
       window.location.reload();
     },
     error: function(error){
+      console.log("Erro.")
+      console.log(error)
       alert("Erro. Por favor tente novamente.\nErro:" + error);
+
     }
-  }); 
+    
+  });
+  return false
 }
