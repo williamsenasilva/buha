f301f96d2864381456c8a5c0c4c9af2c6739d65c
Andre Barbosa <an.barbosa@outlook.com.br>
2018-11-23 04:22:01 -0200

    Fazendo a redistribuição dos arquivos toda vez que um nó entra na rede.
    ERRO! > Nâo sei se é só no meu computador, mas não consigo inserir
    alunos individualmente. Quando clico em Inserir Aluno, preencho o
    formulario e dou o "OK", nada acontece. Coloquei um 'print' na funcao
    insert_student(), na primeira linha, e não funcionou.
    
    Por outro lado, provavelmente a saída de nós também está funcionando
    bem.

--- src/webapp/helper.py
+++ src/webapp/helper.py
@@ -1,7 +1,7 @@
 import os
 import ast
 from models import Student
-from dht import DHT, Node
+
 
 
 
@@ -26,41 +26,6 @@ def update_file(fullpath,newline):
     file = open(fullpath, 'w')
     file.writelines(text)
     file.close()
-
-def student_from_file(fullpath):
-    try:
-        with open(fullpath, 'r') as f:
-            s = f.read()
-            print(s)
-            data = ast.literal_eval(s)
-    except FileNotFoundError:
-        return None
-
-    name = data['NAME']
-    university = data['UNIVERSITY']
-    academic_id = data['ACADEMIC_ID']
-    buha_id = data['BUHA_ID']
-    student = Student({
-                        "name": name, 
-                        "university": university, 
-                        "academicID": academic_id, 
-                        "buha_id":buha_id
-                        })  
-    return student
-
-def make_path_for(dht, session, app, node_id=None, academic_id=None):
-    base_path = app.root_path + "/static/dht/"
-    base_path += session.get('moment') +'/' 
-    if (node_id is None) and (academic_id is None):
-        return base_path
-    if node_id is None:
-        node_ref = dht.find_node(dht.start_node, academic_id)._id
-    else:
-        node_ref = node_id
-    if academic_id is None:
-        return base_path + str(node_ref) + '/'
-    else:
-        return base_path + str(node_ref) + '/' + str(academic_id)+'.txt'      
 
 def make_tree(path):
     tree = dict(name=path, children=[])
